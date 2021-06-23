1f107d25267c735e5f37146f13488fcbd8f4213a
David <59164341+cryzzpie@users.noreply.github.com>
2021-06-23 04:24:25 +0200

    Add files via upload

---
+++ hazenikostkou.py
@@ -0,0 +1,15 @@
+#Hazení kostkou v jazyku python
+#Hello!
+
+
+import random  
+znovu= True
+while znovu:  
+    print("Hodil si" ,random.randint(1,6)) 
+    print("Chceš házet znovu? Ano/Ne")
+    znovu = ("A" or "Ano") in input ()
+
+    #cryzz#9999
+    #github cryzzpie
+   
+         #made for educational purposes, free to use (no clue why would you want to use it anyways :D)
